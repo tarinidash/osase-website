7ae0a1744cf7b4b5bfc9339747faa027136bbfa8
Tarini Dash <dash.tarini@gmail.com>
2017-03-05 12:55:51 -0500

    added mailing service for events

--- src/main/java/com/osa/se/service/EventService.java
+++ src/main/java/com/osa/se/service/EventService.java
@@ -2,22 +2,34 @@ package com.osa.se.service;
 
 import com.osa.se.dao.EventRepository;
 import com.osa.se.model.Event;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 
 /**
  * Created by tarinidash on 2/28/17.
  */
 @Service
+@Slf4j
 public class EventService {
 
     private EventRepository eventRepository;
+    private JavaMailSender javaMailSender;
+    private String notifiedBy;
 
-    public EventService(EventRepository eventRepository) {
+    public EventService(EventRepository eventRepository, JavaMailSender javaMailSender, @Value("${spring.mail.username}") final String notifiedBy) {
         this.eventRepository = eventRepository;
+        this.javaMailSender = javaMailSender;
+        this.notifiedBy = notifiedBy;
     }
 
     public Event save(Event event) {
+        sendMail(event);
+        event.setNotified_by(notifiedBy);
         return eventRepository.save(event);
     }
 
@@ -47,5 +59,13 @@ public class EventService {
 
     public void deleteAll() {
         eventRepository.deleteAll();
+    }
+
+    private void sendMail(Event event){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setText(event.getDescription());
+        message.setSubject(event.getEvent_name());
+        message.setTo(event.getNotified_to());
+        javaMailSender.send(message);
     }
 }
